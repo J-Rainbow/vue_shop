b5849096cbabb2b1bdc6725a5d440d2b135c9409
J-Rainbow <1397194173@qq.com>
2021-06-04 22:28:50 +0800

    登录验证

--- src/components/LoginView.vue
+++ src/components/LoginView.vue
@@ -5,16 +5,29 @@
         <img src="../assets/imgs/head.jpg" alt="" />
       </div>
       <div class="loginFrom">
-        <el-form label-width="80px">
-          <el-form-item label="用户名:">
-            <el-input prefix-icon="el-icon-user"></el-input>
+        <!-- :rules="loginRules" 验证规则 ref="loginFormRef"-->
+        <el-form
+          label-width="80px"
+          :model="loginForm"
+          :rules="loginFormRules"
+          ref="loginFormRef"
+        >
+          <!-- prop="username" 与rules使用-->
+          <el-form-item label="用户名:" prop="username">
+            <el-input
+              prefix-icon="el-icon-user"
+              v-model="loginForm.username"
+            ></el-input>
           </el-form-item>
-          <el-form-item label="密码:">
-            <el-input prefix-icon="el-icon-goods"></el-input>
+          <el-form-item label="密码:" prop="password">
+            <el-input
+              prefix-icon="el-icon-lock"
+              v-model="loginForm.password"
+            ></el-input>
           </el-form-item>
           <el-form-item class="loginBut">
-            <el-button type="primary">登录</el-button>
-            <el-button type="info">注销</el-button>
+            <el-button type="primary" @click="login">登录</el-button>
+            <el-button type="info" @click="reLoginFrom">注销</el-button>
           </el-form-item>
         </el-form>
       </div>
@@ -23,7 +36,45 @@
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      loginForm: {
+        username: "",
+        password: "",
+      },
+      loginFormRules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" }, //trigger: "blur"在鼠标离开时验证
+        ],
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }],
+      },
+    };
+  },
+  methods: {
+    async login() {
+      if (this.loginForm.username == "" || this.loginForm.password == "") {
+        return;
+      }
+      await axios({
+        method: "POST",
+        url: "/api/user/login",
+        params: this.loginForm,
+      }).then((res) => {
+        let msg = res.data.msg;
+        if (msg == 1) {
+          alert("登录成功");
+        } else {
+          alert("登录失败");
+        }
+      });
+    },
+    reLoginFrom() {
+      this.$refs.loginFormRef.resetFields();
+    },
+  },
+};
 </script>
 <style lang="less" scoped>
 .login {
@@ -40,7 +91,7 @@ export default {};
     border-radius: 5px;
     display: flex;
     justify-content: center;
-    box-shadow: 0 0 3px #63B0FF;
+    box-shadow: 0 0 3px #63b0ff;
     .boxImg {
       width: 125px;
       height: 125px;
@@ -59,9 +110,9 @@ export default {};
     position: absolute;
     bottom: 30%;
     width: 30%;
-    .loginBut{
-        display: flex;
-        justify-content: flex-end;
+    .loginBut {
+      display: flex;
+      justify-content: flex-end;
     }
   }
 }
